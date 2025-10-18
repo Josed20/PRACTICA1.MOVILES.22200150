da4cb50b963811caa193ac25b6276c905987d3ef
José Cabrera <22200150@ue.edu.pe>
2025-10-18 08:01:23 -0500

    Initial commit

---
+++ app/src/test/java/dev/josed20/practica1moviles22200150/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package dev.josed20.practica1moviles22200150
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
